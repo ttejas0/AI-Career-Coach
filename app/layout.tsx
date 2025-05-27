c60b77ac61fd299679949be9cc8d1f4e3a3ef511
Tejas <ttejas220@gmail.com>
2025-05-27 13:21:42 +0530

    Clerk auth added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AI Career Coach",
@@ -25,27 +18,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {/* HEADER */}
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          {/* FOOTER */}
-          <footer className="bg-muted/50 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              © {new Date().getFullYear()} AI Career Coach. All rights reserved.
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* HEADER */}
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            {/* FOOTER */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                © {new Date().getFullYear()} AI Career Coach. All rights
+                reserved.
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Header;
